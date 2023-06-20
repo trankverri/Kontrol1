69707059c799fe66893357034aa9b0478b633e40
trankverri <trankverri@vk.com>
2023-06-20 10:40:49 +0300

    Add code versthion 1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
 ﻿Console.Clear();
 Console.WriteLine("Hello, Препод!");
+
+void Metod1(string [] StringArray)
+{
+
+    string[] TempArray= {""};
+
+    foreach (var item in StringArray)
+    {
+        if (item.Length<=3)
+        {
+            TempArray=TempArray.Append(item).ToArray();
+            Console.Write("{0}\t", item);
+        }    
+    }
+}
+
+string[] AR = {"Маi","Даша","М","Дарья","hre","00","qwert","qwe"};
+Metod1(AR);
